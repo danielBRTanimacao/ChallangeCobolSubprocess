dabd6e1c085e8e0567427216d490e27c92aa088d
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-05-15 12:42:33 -0300

    feat: main created post and some exception and backUrubu.cob validations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import subprocess
+from pathlib import Path
 
 app = Flask(__name__)
 
@@ -7,10 +8,56 @@ app = Flask(__name__)
 def deposit_value():
     data = request.get_json()
 
-    if data and "deposit" in data: 
-        subprocess.run(["cd ./subprocess && cobc -x backUrubu.cob"])
-        # enviar para o backUrubu
-        return jsonify({"message": f"Valor foi depositado R${data["deposit"]:.2f}"})
+    if data and "deposit" in data and "days" in data: 
+        try:
+            float(data["deposit"])
+            int(data["days"])
+        except (ValueError, TypeError) as e:
+            return jsonify({"error": "Valores invalidos!"})
+        deposit = [str(data["deposit"]), str(data["days"])]
+        
+        try:
+            COBOL_DIR = Path(__file__).resolve().parent / "subprocess"
+
+            compile_result = subprocess.run(
+                ["cobc", "-x", "backUrubu.cob"],
+                cwd=COBOL_DIR,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True
+            )
+
+            if compile_result.returncode != 0:
+                return jsonify(
+                    {   
+                        "erro": "Erro ao compilar o COBOL",
+                        "detail": compile_result.stderr.strip()
+                    }
+                ), 500
+            
+            response_result = subprocess.run(
+                ["./backUrubu", *deposit],
+                cwd=COBOL_DIR,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True
+            )
+
+            if response_result.returncode != 0:
+                return jsonify(
+                    {   
+                        "erro": "Erro ao executr o COBOL",
+                        "detail": compile_result.stderr.strip()
+                    }
+                ), 500
+            
+            response_final = float(response_result.stdout.strip())
+
+            return jsonify({
+                "message": f"Apos {data['days']} dias, o valor que você vai receber é R${response_final:.2f}"
+            })
+        except TypeError as e:
+            return jsonify({"error": e})
 
     return jsonify({"error": "Não foi possivel depositar o valor!"})
 
--- subprocess/backUrubu.cob
+++ subprocess/backUrubu.cob
@@ -3,19 +3,26 @@
 
        data division.
        working-storage section.
+           01 user-str          PIC x(20).
+           01 days-str          PIC x(20).
            01 user-value        PIC 9(5)V99.
+           01 days              PIC 9(5)V99 value 30.
            01 result            PIC 9(7)V99.
            01 calc1             PIC 9(7)V99.
            01 calc2             PIC 9(7)V99.
-           01 argument          PIC X(20).
 
        procedure division.
-           accept argument from command-line
-           move function numval(argument) to user-value
+           accept user-str from command-line
+           move function numval(user-str) to user-value
 
+           accept days-str from command-line
+           if days-str not = space
+               move function numval(days-str) to days
+           end-if
+               
            compute calc1 = user-value / 100
            compute calc2 = calc1 * 33.33
-           compute result = calc2 * 30
+           compute result = calc2 * days
 
            display result
            stop run.
