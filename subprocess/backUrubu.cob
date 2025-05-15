98bcb6a7279a49e282ccd97507b25abb5e5ea85c
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-05-15 08:41:22 -0300

    feat: process calc

---
+++ subprocess/backUrubu.cob
@@ -0,0 +1,12 @@
+       identification division.
+       program-id. backUrubu.
+
+       data division.
+       working-storage section.
+           
+           01 user-value        PIC 9(5)V99.
+           01 result            PIC 9(7)V99.
+           01 calc-value        PIC 9(7)V99.
+           01 calc-final        PIC 9(7)V99.
+           01 ARGUMENTO         PIC X(20).
+           01 VALOR-NUM         PIC 9(5)V99.
